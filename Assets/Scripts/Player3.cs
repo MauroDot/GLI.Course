7230163c0e362022b73466132440f3def080ee9f
MauroDot <63043182+MauroDot@users.noreply.github.com>
2022-12-28 16:44:13 -0600

    layermasks

---
+++ Assets/Scripts/Player3.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player3 : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask _mask;
+    void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Ray rayOrigin = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+            RaycastHit hitInfo;
+
+            if(Physics.Raycast(rayOrigin, out hitInfo, Mathf.Infinity, 1 << 6 | 1 << 7))
+            {
+                hitInfo.collider.GetComponent<MeshRenderer>().material.color = Color.red;
+            }
+        }
+    }
+}
