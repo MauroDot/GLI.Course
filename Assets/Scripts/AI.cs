babfea4f78b18b9a9a3d2b2e818f36b8f94a3a98
MauroDot <63043182+MauroDot@users.noreply.github.com>
2023-01-04 15:30:44 -0600

    Press Space to move to Random Point

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    //store all waypoints
+    //select a random wayoint
+    //travel to that random waypoint
+    //start is called before the first frame update
+    [SerializeField]
+    GameObject[] _wayPoints;
+    private NavMeshAgent _agent;
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+   
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            var randoTarget = Random.Range(0, _wayPoints.Length);
+            _agent.destination = _wayPoints[randoTarget].GetComponent<Transform>().position;
+            Debug.Log($"Destination: {randoTarget}");
+        }
+    }
+}
